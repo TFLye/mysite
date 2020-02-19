16a7c24c033a4c98009342c5c14dd9f373f96f5f
TFLye <Tfenglin@163.com>
2020-02-19 22:26:02 +0800

    test-v

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd)$yuw=+c19242!vquich3@ir0q-y-$1hro9mwp$$8pszh928i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
