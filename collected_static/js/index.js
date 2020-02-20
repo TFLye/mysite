e1c6655c14ea3073916f8c6f8026ba8210e159ff
TFLye <Tfenglin@163.com>
2020-02-20 22:34:39 +0800

    alter static 2

---
+++ collected_static/js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+	//导航栏变化动画
+			$(window).scroll(function () {
+			     if ($(document).scrollTop() > 80) {
+					 $(".top").animate({width: "show"},1000); //IE,FF
+					
+			      }else {
+					  $(".top").animate({width: "hide"},1000); //IE,FF
+					 }
+			});
+		$(function(){
+			$("#toTop").click(function(){
+			$("html").animate({"scrollTop": "0px"},500); //IE,FF
+			$("body").animate({"scrollTop": "0px"},500); //Webkit
+        });
+     })
+});
+
+
+$(function(){
+	if(navigator.userAgent.match(/mobile/i)) {
+		top.location='http://m.baidu.com/';
+	}
+   });
+
+
+ 
--- mysite/urls.py
+++ mysite/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('article/', include('article.urls', namespace='article')),
     path('comment/', include('comment.urls', namespace='comment')),
     # path函数将url映射到视图
+
 ]
